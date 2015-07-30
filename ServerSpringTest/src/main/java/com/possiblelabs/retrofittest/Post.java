ffffd3b646525aed0913a100f1a4e24d60b6bc85
Alvaro Orellana <tiveor@gmail.com>
2015-07-30 11:31:33 -0400

    ServerSpringTest

---
+++ ServerSpringTest/src/main/java/com/possiblelabs/retrofittest/Post.java
@@ -0,0 +1,86 @@
+package com.possiblelabs.retrofittest;
+
+/**
+ * Created by possiblelabs on 7/30/15.
+ */
+public class Post {
+    private String userId;
+    private String id;
+    private String title;
+    private String body;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Post{");
+        sb.append("userId='").append(userId).append('\'');
+        sb.append(", id='").append(id).append('\'');
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", body='").append(body).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    private boolean isNull(String s) {
+        return s == null || s.isEmpty();
+    }
+
+    public void updatePost(Post post) {
+        if (!isNull(post.userId))
+            this.userId = post.userId;
+
+        if (!isNull(post.id))
+            this.id = post.id;
+
+        if (!isNull(post.title))
+            this.title = post.title;
+
+        if (!isNull(post.body))
+            this.body = post.body;
+    }
+
+    public void modifyPost(Post post) {
+        if (!isNull(this.userId) && !isNull(post.userId))
+            this.userId = post.userId;
+
+        if (!isNull(this.id) && !isNull(post.id))
+            this.id = post.id;
+
+        if (!isNull(this.title) && !isNull(post.title))
+            this.title = post.title;
+
+        if (!isNull(this.body) && !isNull(post.body))
+            this.body = post.body;
+    }
+}
---
+++ ServerSpringTest/src/main/java/com/possiblelabs/retrofittest/PostController.java
@@ -0,0 +1,75 @@
+package com.possiblelabs.retrofittest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by possiblelabs on 7/30/15.
+ */
+@RestController
+public class PostController {
+
+
+    private static List<Post> posts = new ArrayList<Post>();
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public @ResponseBody List<Post> listPosts(@RequestParam(value = "order", defaultValue = "asc") String order) {
+        return posts;
+    }
+
+    @RequestMapping(value = "/posts", method = RequestMethod.POST)
+    public @ResponseBody Post addPost(@RequestBody Post post){
+        post.setId(counter.incrementAndGet() +"");
+        posts.add(post);
+        return posts.get(posts.size() -1);
+    }
+
+    private Post getPostById(String id){
+        for(Post post: posts){
+            if(post.getId().equals(id)){
+                return post;
+            }
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public @ResponseBody Post getPost(@PathVariable String id){
+        return getPostById(id);
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.PUT)
+    public @ResponseBody Post updatePost(@PathVariable String id, @RequestBody Post post){
+        Post ref = getPostById(id);
+        if(ref != null)
+            ref.updatePost(post);
+        return ref;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.PATCH)
+    public @ResponseBody Post modifyPost(@PathVariable String id, @RequestBody Post post){
+        Post ref = getPostById(id);
+        if(ref != null)
+            ref.modifyPost(post);
+        return ref;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.DELETE)
+    public @ResponseBody Post deletePost(@PathVariable String id){
+        Post ref = getPostById(id);
+        if(ref != null)
+            posts.remove(ref);
+        return ref;
+    }
+
+}
