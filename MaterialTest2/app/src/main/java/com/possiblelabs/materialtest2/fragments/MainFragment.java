c323f4616ab41147f8eb87bb06c92aa7eb917282
Alvaro Orellana <tiveor@gmail.com>
2015-11-28 10:55:01 -0400

    MaterialTest Part 1

---
+++ MaterialTest2/app/src/main/java/com/possiblelabs/materialtest2/fragments/MainFragment.java
@@ -0,0 +1,166 @@
+package com.possiblelabs.materialtest2.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.possiblelabs.materialtest2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MainFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MainFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MainFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MainFragment newInstance(String param1, String param2) {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        EditText editTextMaterial1 = (EditText) root.findViewById(R.id.editTextMaterial1);
+        EditText editTextMaterial2 = (EditText) root.findViewById(R.id.editTextMaterial2);
+
+
+        TextInputLayout textInputLayout1 = (TextInputLayout) root.findViewById(R.id.textInputLayout1);
+
+        editTextMaterial1.addTextChangedListener(new LoginWatcher(textInputLayout1, editTextMaterial1));
+
+        EditText editTextNormal1 = (EditText) root.findViewById(R.id.editTextNormal1);
+        EditText editTextNormal2 = (EditText) root.findViewById(R.id.editTextNormal2);
+        editTextNormal2.setError("Error en el password");
+
+        return root;
+
+
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    public class LoginWatcher implements TextWatcher {
+
+        private TextInputLayout inputLayout;
+        private EditText editText;
+
+        public LoginWatcher(TextInputLayout inputLayout, EditText editText) {
+            this.inputLayout = inputLayout;
+            this.editText = editText;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String inputText = editText.getText().toString();
+            Log.d("TEXT", inputText);
+            if (inputText.contains("@") && inputText.contains(".")) {
+                inputLayout.setErrorEnabled(false);
+
+            } else {
+                inputLayout.setError("Error al escribir el texto");
+                inputLayout.setErrorEnabled(true);
+            }
+
+        }
+    }
+}
