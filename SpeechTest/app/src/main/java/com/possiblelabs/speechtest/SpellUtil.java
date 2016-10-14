b1a6fe5374f58045a3d594581a606678a99d88fb
Alvaro Orellana <tiveor@gmail.com>
2016-10-13 22:21:18 -0400

    Refactoring

--- SpeechTest/app/src/main/java/com/possiblelabs/speechtest/SpellUtil.java
+++ SpeechTest/app/src/main/java/com/possiblelabs/speechtest/SpellUtil.java
@@ -9,68 +9,11 @@ public class SpellUtil {
     public static String convertToSpellOnce(String words) {
         StringBuilder sb = new StringBuilder();
         for (char letter : words.toCharArray()) {
-            sb.append(convert(letter));
+            sb.append(letter);
+            //sb.append(convertToSpellOnce(letter)); this is another option
             sb.append(",");
         }
         return sb.toString();
-    }
-
-    private static String convert(char letter) {
-        switch (letter) {
-            case 'a':
-                return "a";
-            case 'b':
-                return "b";
-            case 'c':
-                return "c";
-            case 'd':
-                return "d";
-            case 'e':
-                return "e";
-            case 'f':
-                return "f";
-            case 'g':
-                return "g";
-            case 'h':
-                return "h";
-            case 'i':
-                return "i";
-            case 'j':
-                return "j";
-            case 'k':
-                return "k";
-            case 'l':
-                return "l";
-            case 'm':
-                return "m";
-            case 'n':
-                return "n";
-            case 'o':
-                return "o";
-            case 'p':
-                return "p";
-            case 'q':
-                return "q";
-            case 'r':
-                return "r";
-            case 's':
-                return "s";
-            case 't':
-                return "t";
-            case 'u':
-                return "u";
-            case 'v':
-                return "v";
-            case 'w':
-                return "w";
-            case 'x':
-                return "x";
-            case 'y':
-                return "y";
-            case 'z':
-                return "z";
-        }
-        return "";
     }
 
     private static String convertSoundBased(char letter) {
